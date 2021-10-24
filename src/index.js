5e901ce8ba0bfbf5c696ebf764dd02791427fd93
Josh <thisisjoshford@gmail.com>
2021-10-24 10:50:15 +0200

    simplify keystore & add comments

--- src/index.js
+++ src/index.js
@@ -1,49 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import getConfig from './config.js';
-import * as nearAPI from 'near-api-js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import getConfig from "./config.js";
+import * as nearAPI from "near-api-js";
 
 // Initializing contract
 async function initContract() {
-  const nearConfig = getConfig(process.env.NODE_ENV || 'testnet');
+  // get network configuration values from config.js
+  // based on the network ID we pass to getConfig()
+  const nearConfig = getConfig(process.env.NODE_ENV || "testnet");
+  // create a keyStore for signing transactions using the user's key
+  // which is located in the browser local storage after user logs in
+  const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
 
-  // Initializing connection to the NEAR TestNet
-  const near = await nearAPI.connect({
-    deps: {
-      keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore()
-    },
-    ...nearConfig
-  });
+  // Initializing connection to the NEAR testnet
+  const near = await nearAPI.connect({ keyStore, ...nearConfig });
 
-  // Needed to access wallet
+  // Initialize wallet connection
   const walletConnection = new nearAPI.WalletConnection(near);
 
-  // Load in account data
+  // Load in user's account data
   let currentUser;
-  if(walletConnection.getAccountId()) {
+  if (walletConnection.getAccountId()) {
     currentUser = {
       accountId: walletConnection.getAccountId(),
-      balance: (await walletConnection.account().state()).amount
+      balance: (await walletConnection.account().state()).amount,
     };
   }
 
   // Initializing our contract APIs by contract name and configuration
-  const contract = await new nearAPI.Contract(walletConnection.account(), nearConfig.contractName, {
-    // View methods are read-only – they don't modify the state, but usually return some value
-    viewMethods: ['getMessages'],
-    // Change methods can modify the state, but you don't receive the returned value when called
-    changeMethods: ['addMessage'],
-    // Sender is the account ID to initialize transactions.
-    // getAccountId() will return empty string if user is still unauthorized
-    sender: walletConnection.getAccountId()
-  });
+  const contract = await new nearAPI.Contract(
+    walletConnection.account(),
+    nearConfig.contractName,
+    {
+      // View methods are read-only – they don't modify the state, but usually return some value
+      viewMethods: ["getMessages"],
+      // Change methods can modify the state, but you don't receive the returned value when called
+      changeMethods: ["addMessage"],
+      // Sender is the account ID to initialize transactions.
+      // getAccountId() will return empty string if user is still unauthorized
+      sender: walletConnection.getAccountId(),
+    }
+  );
 
   return { contract, currentUser, nearConfig, walletConnection };
 }
 
-window.nearInitPromise = initContract()
-  .then(({ contract, currentUser, nearConfig, walletConnection }) => {
+window.nearInitPromise = initContract().then(
+  ({ contract, currentUser, nearConfig, walletConnection }) => {
     ReactDOM.render(
       <App
         contract={contract}
@@ -51,6 +55,7 @@ window.nearInitPromise = initContract()
         nearConfig={nearConfig}
         wallet={walletConnection}
       />,
-      document.getElementById('root')
+      document.getElementById("root")
     );
-  });
+  }
+);

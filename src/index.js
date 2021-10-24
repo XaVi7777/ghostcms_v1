3332c02948e0aa9f95420d0be6dc8adb729076d2
Josh <thisisjoshford@gmail.com>
2021-10-24 11:19:05 +0200

    add comments regarding user account data

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ async function initContract() {
   // get network configuration values from config.js
   // based on the network ID we pass to getConfig()
   const nearConfig = getConfig(process.env.NODE_ENV || "testnet");
+
   // create a keyStore for signing transactions using the user's key
   // which is located in the browser local storage after user logs in
   const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
@@ -23,7 +24,9 @@ async function initContract() {
   let currentUser;
   if (walletConnection.getAccountId()) {
     currentUser = {
+      // Gets the accountId as a string
       accountId: walletConnection.getAccountId(),
+      // Gets the user's token balance
       balance: (await walletConnection.account().state()).amount,
     };
   }
